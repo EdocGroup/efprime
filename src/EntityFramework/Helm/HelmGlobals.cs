7b72b36ed7273da41cab97063ace79146e177151
chris sand <chris.sand@edocgroup.com>
2016-06-09 14:29:01 -0700

    suppress helm stuff for 6.1.3 dll builds

--- src/EntityFramework/Helm/HelmGlobals.cs
+++ src/EntityFramework/Helm/HelmGlobals.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace System.Data.Entity.Helm
 {
+#pragma warning disable 1591
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Globals")]
     public static class HelmGlobals
     {
         public static HelmConnectionType ConnectionType { get; set; }
@@ -13,7 +15,10 @@ namespace System.Data.Entity.Helm
 
     public enum HelmConnectionType
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "SQLCE")]
         SQLCE,
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "SQL")]
         SQLServer
     }
+#pragma warning restore 1591
 }
